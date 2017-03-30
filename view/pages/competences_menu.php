7ac770c1a7e8c08db63e284ad4c0462ff9b8a005
S.Rebouh <rebouh.sarah@gmail.com>
2017-03-30 10:22:42 +0200

    home images

---
+++ view/pages/competences_menu.php
@@ -0,0 +1,12 @@
+<section>
+  <div class="row" id="menuCompet">
+    <div class="" id="bulle1">
+      <p>Journalisme</p>
+    </div>
+    <div class="" id="bulle2">
+      <p>Développement web</p>
+    </div>
+  </div>
+
+
+</section>
