07e6d9389553c0700e3de3d9ffbd363e919df24d
S.Rebouh <rebouh.sarah@gmail.com>
2017-04-13 16:56:58 +0200

    onglets hashtag

--- view/index.php
+++ view/index.php
@@ -6,22 +6,73 @@
 </div>
 
 <div class="content">
-<div class="container">
-  <img id="me" class="col-xs-12" src="view/images/sarahrebouh.png" alt="">
+    <div class="container">
+      <img id="me" class="col-xs-12" src="view/images/sarahrebouh.png" alt="">
 
 
-    <div class="col-xs-12" id="hashtag">
-      <p>{{presse}}</p>
-      <p>{{com}}</p>
-      <p>{{social}}</p>
-      <p>{{photo}}</p>
-      <p>{{voyage}}</p>
-      <p>{{musique}}</p>
-      <p>{{gastronomie}}</p>
-      <p>{{sport}}</p>
-      <p>{{humour}}</p>
-      <p>{{polyvalence}}</p>
+        <div class="col-xs-12" id="hashtag">
+          <a href="#"><p>{{presse}}</p></a>
+          <a href="#"><p>{{com}}</p></a>
+          <a href="#"><p>{{social}}</p></a>
+          <a href="#"><p>{{photo}}</p></a>
+          <a href="#"><p>{{voyage}}</p></a>
+          <a href="#"><p>{{musique}}</p></a>
+          <a href="#"><p>{{gastronomie}}</p></a>
+          <a href="#"><p>{{sport}}</p></a>
+          <a href="#"><p>{{humour}}</p></a>
+          <a href="#"><p>{{polyvalence}}</p></a>
+        </div>
+
+        <div class="presse">
+            <h2>#Presse</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        </div>
+        <div class="com">
+            <h2>#Communication</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        </div>
+        <div class="social">
+            <h2>#RéseauxSociaux</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        </div>
+        <div class="photo">
+            <h2>#Photo</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        </div>
+        <div class="voyage">
+            <h2>#Voyages</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        </div>
+        <div class="musique">
+            <h2>#Musique</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        </div>
+        <div class="gastronomie">
+            <h2>#Gastronomie</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        </div>
+        <div class="sport">
+            <h2>#Football #Ski #Randonnée</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        </div>
+        <div class="humour">
+            <h2>#Humour #Convivialité</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        </div>
+        <div class="polyvalence">
+            <h2>#Polyvalence #Défis</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        </div>
     </div>
-</div>
 </div>
 <script src="view/js/animations.js" type="text/javascript"></script>
