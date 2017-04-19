16a070d4f527e91447792d484a7fe80b9ee43793
S.Rebouh <rebouh.sarah@gmail.com>
2017-04-19 17:17:44 +0200

    css et réglages

--- view/pages/portfolio_journalisme.php
+++ view/pages/portfolio_journalisme.php
@@ -7,8 +7,8 @@
           foreach ($req as $row){
             echo "<div class='blockPort'><h4>".$row["titre"]."</h4>" ;
             echo "<p>".$row["description"]."</p>" ;
-            echo "<a class='lienPort' href=".$row["lien"].">► Voir le projet</a>" ;
-            echo "<img id='imgPort' src='view/images/".$row["img"]."'><br></div>" ;
+            echo "<a class='lienPort' href=".$row["lien"].">► Voir l'article</a>" ;
+            echo "<img class='img-responsive' id='imgPort' src='view/images/".$row["img"]."'><br></div>" ;
          } ?>
 
     </div>
--- view/templates/footer.php
+++ view/templates/footer.php
@@ -3,22 +3,22 @@
         <div class="row" id="foot">
             <div class="col-xs-3">
                 <a class="icon" id="competences" href="index.php?param_url=competences_menu">
-                    <img class="" src="view/images/competence.png" alt="">Compétences
+                    <img class="compet" src="view/images/competence.png" alt=""><p>Compétences</p>
                 </a>
             </div>
             <div class="col-xs-3">
                 <a class="icon" id="portfolio" href="index.php?param_url=portfolio_menu">
-                    <img class="" src="view/images/portfolio.png" alt="">Portfolio
+                    <img class="" src="view/images/portfolio.png" alt=""><p>Portfolio</p>
                 </a>
             </div>
             <div class="col-xs-3">
                 <a class="icon" id="diplomes" href="index.php?param_url=diplomes">
-                    <img class="" src="view/images/diplome.png" alt="">Diplômes
+                    <img class="" src="view/images/diplome.png" alt=""><p>Diplômes</p>
                 </a>
             </div>
             <div class="col-xs-3">
                 <a class="icon" id="contact" href="index.php?param_url=contact">
-                    <img class="" src="view/images/contact.png" alt="">Contact
+                    <img class="" src="view/images/contact.png" alt=""><p>Contact</p>
                 </a>
             </div>
         </div>
